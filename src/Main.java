d4dd7a8078caddf9f491ac4fe6e9b79c26f0452c
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2022-10-11 03:01:52 +0800

    Add 1st task solution

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,34 @@
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
+    private static Random rand = new Random();
 
     public static void main(String[] args)
     {
         String choice;
         do {
             ClearConsole();
-
-            System.out.println("Выберите дальнейшее действие:");
-            System.out.println("[A] - Запустить решение задачи A;");
-            System.out.println("[B] - Запустить решение задачи B;");
-            System.out.println("[C] - Запустить решение задачи C;");
-            System.out.println("[Q] - Выйти из программы.");
-            System.out.println();
-            System.out.print("Ваш выбор: ");
+            PrintMenu();
 
             choice = scanner.nextLine().toLowerCase(Locale.ROOT);
 
             ClearConsole();
             switch (choice) {
                 case "a":
-                    System.out.println("Решаем задачу A");
+                    RunTask1();
                     break;
 
                 case "b":
-                    System.out.println("Решаем задачу B");
+                    RunTask2();
                     break;
 
                 case "c":
-                    System.out.println("Решаем задачу C");
+                    RunTask3();
                     break;
 
                 default:
@@ -56,5 +52,97 @@ public class Main {
     {
         System.out.println("\nНажмите Enter, чтобы продолжить");
         scanner.nextLine();
+    }
+
+    private static void PrintMenu()
+    {
+        System.out.println("Выберите дальнейшее действие:");
+        System.out.println("[A] - Запустить решение задачи A;");
+        System.out.println("[B] - Запустить решение задачи B;");
+        System.out.println("[C] - Запустить решение задачи C;");
+        System.out.println("[Q] - Выйти из программы.");
+        System.out.println();
+        System.out.print("Ваш выбор: ");
+    }
+
+
+    private static void RunTask1()
+    {
+        Integer size;
+        do {
+            ClearConsole();
+            System.out.print("Введите размер массива N (1 < N < 10): ");
+
+            size = scanner.nextInt();
+        }
+        while (size <= 1 || size >= 10);
+
+        ArrayList<Double> source_array = new ArrayList<>();
+
+        for (int i = 0; i < size; ++i) {
+            Double new_el = Math.round(
+                (rand.nextDouble() * 100.0 - 50.0) * 10.0
+            ) / 10.0;
+
+            source_array.add(new_el);
+        }
+
+        System.out.println("Сгенерированный массив:");
+        System.out.println(source_array);
+
+
+        System.out.print("\nВведите второй операнд: ");
+        Double arg = scanner.nextDouble();
+
+        System.out.println("\n\n");
+
+        String op_string;
+        do {
+            System.out.print("\033[1A\r\033[J");
+            System.out.print("Введите знак операции (+,-,*,/): ");
+
+            op_string = scanner.nextLine();
+        }
+        while (!op_string.matches("[\\+\\-\\*/]"));
+        Character op = op_string.charAt(0);
+
+
+        ArrayList<Double> new_array = new ArrayList<>();
+
+        for (Double el : source_array) {
+            Double new_el = 0.0;
+            switch (op) {
+                case '+':
+                    new_el = el + arg;
+                    break;
+
+                case '-':
+                    new_el = el - arg;
+                    break;
+
+                case '*':
+                    new_el = el * arg;
+                    break;
+
+                case '/':
+                    new_el = el / arg;
+                    break;
+
+            }
+            new_array.add(new_el);
+        }
+
+        System.out.println("\nОбработанный массив:");
+        System.out.println(new_array);
+    }
+
+    private static void RunTask2()
+    {
+        System.out.println("Решаем задачу B");
+    }
+
+    private static void RunTask3()
+    {
+        System.out.println("Решаем задачу C");
     }
 }
